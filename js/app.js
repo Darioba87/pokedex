b71d3143c9269fbc3c81f4350979952958f49c43
Darioba87 <dariobrito87@hotmail.com>
2024-11-08 08:46:13 +0100

    stability improvements V1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 import { init } from "./modules/main.js";
 import { getRespNavbar, getListButton } from "./modules/menu.js";
 
-
 getRespNavbar();
 getListButton();
 
-init();
+if (window.location.href.includes("index.html")) {
+  init();
+}
